cf9548d47c851e6d23fc427a78f6662f7e00e5c0
Henry Arias <h.arias.c3@gmail.com>
2024-05-23 00:41:09 -0500

    Creación y configuración inicial del proyecto.

---
+++ app/src/main/java/com/inmobixpress/inmobixpress/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.inmobixpress.inmobixpress
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.inmobixpress.inmobixpress.ui.theme.InmobiXpressAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            InmobiXpressAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    InmobiXpressAppTheme {
+        Greeting("Android")
+    }
+}
